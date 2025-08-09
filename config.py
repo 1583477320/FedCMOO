a28cf4e0e76aef3c77bacb11e8688f4939c4f60e
Yuelei <66400355+1583477320@users.noreply.github.com>
2025-08-09 20:05:09 +0800

    Update config.py

--- config.py
+++ config.py
@@ -221,7 +221,7 @@ def base_config_set(base_config_file_path, experiment, algorithm):
             d["hyperparameters"]["local_training"]["local_lr"] = 0.3
         elif algorithm == "fedadam":
             d["hyperparameters"]["global_lr"] = 1.1
-            d["hyperparameters"]["local_training"]["local_lr"] = 0.1
+            d["hyperparameters"]["local_training"]["local_lr"] = 0.01
             
         
     elif experiment == "MNIST_FMNIST":
