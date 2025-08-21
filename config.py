d4070011e3f40ef7ba80c1ea14e891ffc585e6c7
Yuelei <66400355+1583477320@users.noreply.github.com>
2025-08-21 11:03:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -237,7 +237,7 @@ def base_config_set(base_config_file_path, experiment, algorithm):
 
     elif experiment == "MNIST_FMNIST":
         if algorithm == "fsmgda":
-            d["hyperparameters"]["global_lr"] = 2
+            d["hyperparameters"]["global_lr"] = 1.5
             d["hyperparameters"]["local_training"]["local_lr"] = 0.1
         elif algorithm == "fedcmoo":
             d["hyperparameters"]["global_lr"] = 1.2
@@ -246,8 +246,8 @@ def base_config_set(base_config_file_path, experiment, algorithm):
             d["hyperparameters"]["global_lr"] = 1.6
             d["hyperparameters"]["local_training"]["local_lr"] = 0.3
         elif algorithm == "fsmgda_vr":
-            d["hyperparameters"]["global_lr"] = 1.6
-            d["hyperparameters"]["local_training"]["local_lr"] = 0.25
+            d["hyperparameters"]["global_lr"] = 1.5
+            d["hyperparameters"]["local_training"]["local_lr"] = 0.1
             
 
     elif experiment == "CIFAR10_MNIST":
