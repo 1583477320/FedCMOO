7365d6053cee7f760b19d6813d6718d6dc7ee976
Yuelei <66400355+1583477320@users.noreply.github.com>
2025-08-24 00:19:47 +0800

    Update server.py

--- server.py
+++ server.py
@@ -249,7 +249,7 @@ class Server(object):
         for self.round_num in range(start_round, self.config['max_round']):
             if self.config["hyperparameters"]["local_training"]["local_lr_scheduler_flag"]:  # LR scheduler
                 # Check if the current round is a multiple of the decay interval
-                if self.round_num % (self.config['max_round'] // 5) == 0 and self.round_num != 0:
+                if self.round_num % 25 == 0 and self.round_num != 0:
                     # Halve the learning rate
                     new_lr = self.config["hyperparameters"]["local_training"]["local_lr"] * 0.5
                     self.config["hyperparameters"]["local_training"]["local_lr"] = new_lr
