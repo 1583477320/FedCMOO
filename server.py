4bf3a23521bc9d6e301c70a6bff5c14354a6e23e
Yuelei <66400355+1583477320@users.noreply.github.com>
2025-09-01 16:52:36 +0800

    Update server.py

--- server.py
+++ server.py
@@ -261,7 +261,7 @@ class Server(object):
                     logging.info(f"Round {self.round_num}: Adjusting learning rate to {new_lr:.6f}")
                 elif self.round_num % 30 == 0 and 51 <= self.round_num < 101:
                     # Halve the learning rate
-                    new_lr = self.config["hyperparameters"]["local_training"]["local_lr"] * 0.009
+                    new_lr = self.config["hyperparameters"]["local_training"]["local_lr"] * 1.009
                     self.config["hyperparameters"]["local_training"]["local_lr"] = new_lr
                     logging.info(f"Round {self.round_num}: Adjusting learning rate to {new_lr:.6f}")
                             
