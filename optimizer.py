1cde9ca9e1091acf85bee8eb923d17d478a75c67
Yuelei <66400355+1583477320@users.noreply.github.com>
2025-09-02 19:58:30 +0800

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -167,7 +167,7 @@ def lr_scheduler(config, round_num):
     elif config["experiment"] == 'MNIST_FMNIST':  # LR scheduler
         if round_num % 22 == 0 and round_num != 0 and round_num < 31:
             # Halve the learning rate
-            new_lr = config["hyperparameters"]["local_training"]["local_lr"] * 0.50
+            new_lr = config["hyperparameters"]["local_training"]["local_lr"] * 0.30
             config["hyperparameters"]["local_training"]["local_lr"] = new_lr
             logging.info(f"Round {round_num}: Adjusting learning rate to {new_lr:.6f}")
         elif round_num % 30 == 0 and 21 <= round_num < 51:
