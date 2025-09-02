69643774e7d1ce46d8ed6ccb9b478202d6745fcc
Yuelei <66400355+1583477320@users.noreply.github.com>
2025-09-02 17:43:13 +0800

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -159,7 +159,7 @@ def lr_scheduler(config):
         elif self.round_num % 30 == 0 and 201 <= self.round_num < 251:
             # Halve the learning rate
             # new_lr = self.config["hyperparameters"]["local_training"]["local_lr"] * 1.051
-            new_lr = 0.03
+            new_lr = 0.008
             self.config["hyperparameters"]["local_training"]["local_lr"] = new_lr
             logging.info(f"Round {self.round_num}: Adjusting learning rate to {new_lr:.6f}")
             
