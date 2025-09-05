4ec1c26a7c2d2e8cf32834adc103760a7309ab2a
Yuelei <66400355+1583477320@users.noreply.github.com>
2025-09-05 20:32:26 +0800

    Update config.py

--- config.py
+++ config.py
@@ -248,7 +248,7 @@ def base_config_set(base_config_file_path, experiment, algorithm):
             d["hyperparameters"]["local_training"]["local_lr"] = 0.3
         elif algorithm == "fsmgda_vr":
             d["hyperparameters"]["global_lr"] = 1.5
-            d["hyperparameters"]["local_training"]["local_lr"] = 0.25
+            d["hyperparameters"]["local_training"]["local_lr"] = 0.025
             d["hyperparameters"]["local_training"]["local_lr_scheduler_flag"] = False
     
         
