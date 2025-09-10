821fc88cca7004a0b9542613bc7cfe8c46e4c9c6
Yuelei <66400355+1583477320@users.noreply.github.com>
2025-09-10 15:17:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -260,7 +260,7 @@ def base_config_set(base_config_file_path, experiment, algorithm):
     elif experiment == "CIFAR10_MNIST":
         if algorithm == "fsmgda_vr":
             d["hyperparameters"]["global_lr"] = 1.6
-            d["hyperparameters"]["local_training"]["local_lr"] = 0.35
+            d["hyperparameters"]["local_training"]["local_lr"] = 0.30
             d["hyperparameters"]["local_training"]["local_lr_scheduler_flag"] = False
         else:
             d["hyperparameters"]["global_lr"] = 0.1
