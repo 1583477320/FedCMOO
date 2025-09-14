5e5d73e110ffc3c56674770824ff87617f9f2da5
Yuelei <66400355+1583477320@users.noreply.github.com>
2025-09-14 20:46:20 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1001,9 +1001,7 @@ class Client(object):
                             out, _ = global_model[task](rep, None)
                             loss = loss_fn[task](out, labels)
                             loss.backward()
-                            print("out stats:", out.min().item(), out.max().item())
-                            print("loss:", loss.item())
-
+                            
                             # 计算上轮次模型的梯度
                             rep, _ = kwargs['last_model']['rep'](images, None)
                             out, _ = kwargs['last_model'][task](rep, None)
