6e959bcbd6ce18606e414455e42123fdadacf887
Yuelei <66400355+1583477320@users.noreply.github.com>
2025-09-18 21:46:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -241,12 +241,12 @@ def base_config_set(base_config_file_path, experiment, algorithm):
 
     elif experiment == "MNIST_FMNIST":
         if algorithm == "fsmgda":
-            d["hyperparameters"]["global_lr"] = 1.8
-            d["hyperparameters"]["local_training"]["local_lr"] = 0.3
+            d["hyperparameters"]["global_lr"] = 2
+            d["hyperparameters"]["local_training"]["local_lr"] = 0.1
             d["hyperparameters"]["local_training"]["local_lr_scheduler_flag"] = False
         elif algorithm == "fedcmoo":
             d["hyperparameters"]["global_lr"] = 1.2
-            d["hyperparameters"]["local_training"]["local_lr"] = 0.1
+            d["hyperparameters"]["local_training"]["local_lr"] = 0.5
             d["hyperparameters"]["local_training"]["local_lr_scheduler_flag"] = False
         elif algorithm == "fedcmoo_pref":
             d["hyperparameters"]["global_lr"] = 1.6
