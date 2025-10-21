90f6ee891d213ae8e63965c51105c91a4f35941f
Yuelei <66400355+1583477320@users.noreply.github.com>
2025-10-21 08:49:51 +0800

    Update config.py

--- config.py
+++ config.py
@@ -246,7 +246,7 @@ def base_config_set(base_config_file_path, experiment, algorithm):
             d["hyperparameters"]["local_training"]["local_lr_scheduler_flag"] = False
         elif algorithm == "fedcmoo":
             d["hyperparameters"]["global_lr"] = 1.2
-            d["hyperparameters"]["local_training"]["local_lr"] = 0.3
+            d["hyperparameters"]["local_training"]["local_lr"] = 0.2
             d["hyperparameters"]["local_training"]["local_lr_scheduler_flag"] = False
         elif algorithm == "fedcmoo_pref":
             d["hyperparameters"]["global_lr"] = 1.6
